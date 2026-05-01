9b3a4fae079f5466dee6330d1db4da692c812a08
Josefh Gabriel Argüello Ramos <josefharue@gmail.com>
2026-05-01 14:05:56 -0600

    Modelo de Empleado con DbContext

---
+++ Tarea3_JosefhArguello/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tarea3_JosefhArguello.Models;
+
+namespace Tarea3_JosefhArguello.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
