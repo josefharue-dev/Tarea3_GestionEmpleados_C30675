c408ab680310b9423fc36317edd1594c79ac5a1c
josef <josef@MSI-KATANA-17>
2026-05-01 13:44:57 -0600

    Agregar archivos de proyecto.

---
+++ Tarea3_JosefhArguello/Models/Empleado.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tarea3_JosefhArguello.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(80, ErrorMessage = "Máximo 80 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Los apellidos son obligatorios")]
+        [StringLength(100, ErrorMessage = "Máximo 100 caracteres")]
+        public string Apellidos { get; set; }
+
+        [Required(ErrorMessage = "El departamento es obligatorio")]
+        public string Departamento { get; set; }
+
+        [Required(ErrorMessage = "El salario es obligatorio")]
+        [Range(400000, 10000000, ErrorMessage = "El salario debe estar entre 400000 y 10000000")]
+        public decimal Salario { get; set; }
+
+        public DateTime FechaIngreso { get; set; } = DateTime.Now;
+
+        public bool Activo { get; set; } = true;
+
+
+        public string NombreCompleto
+        {
+            get { return $"{Nombre} {Apellidos}"; }
+        }
+    }
+}
